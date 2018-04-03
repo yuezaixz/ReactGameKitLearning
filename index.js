753b86ea8e37d0e039dce1bb69b24b4be386e9f7
吴迪玮 <David_Woo@wudiweideMacBook-Pro-2.local>
2018-04-03 10:45:35 +0800

    🏀

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import Game from './src';
 
-AppRegistry.registerComponent('ReactGameKitLearning', () => App);
+// AppRegistry.registerComponent('ReactGameKitLearning', () => App);
+AppRegistry.registerComponent('ReactGameKitLearning', () => Game);
