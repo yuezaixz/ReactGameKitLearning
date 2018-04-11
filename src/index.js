23fc2d6d298d16e5acab491d2dcd6c3c79543102
吴迪玮 <xiao303178394@gmail.com>
2018-04-11 17:31:05 +0800

    篮框

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ export default class Game extends Component {
         const dimensions = Dimensions.get('window');
 
         const ground = Matter.Bodies.rectangle(
-            dimensions.width / 2, dimensions.height + 5,
+            dimensions.width / 2, dimensions.height + 5-75/2,
             dimensions.width, 5,
             {
                 isStatic: true,
@@ -31,7 +31,7 @@ export default class Game extends Component {
         );
 
         const ceiling = Matter.Bodies.rectangle(
-            dimensions.width / 2, -75,
+            dimensions.width / 2, -75/2,
             dimensions.width, 1,
             {
                 isStatic: true,
@@ -39,7 +39,7 @@ export default class Game extends Component {
         );
 
         const leftWall = Matter.Bodies.rectangle(
-            -75, dimensions.height / 2,
+            -75/2, dimensions.height / 2,
             1, dimensions.height,
             {
                 isStatic: true,
@@ -47,14 +47,30 @@ export default class Game extends Component {
         );
 
         const rightWall = Matter.Bodies.rectangle(
-            dimensions.width, dimensions.height / 2,
+            dimensions.width-75/2, dimensions.height / 2,
             1, dimensions.height - 5,
             {
                 isStatic: true,
             },
         );
 
-        Matter.World.add(engine.world, [ground, leftWall, rightWall, ceiling]);
+        const frontBacket = Matter.Bodies.rectangle(
+            dimensions.width-195, 195,
+            5, 5,
+            {
+                isStatic: true,
+            },
+        );
+
+        const backBacket = Matter.Bodies.rectangle(
+            dimensions.width-50, 195,
+            50, 5,
+            {
+                isStatic: true,
+            },
+        );
+
+        Matter.World.add(engine.world, [ground, leftWall, rightWall, ceiling, frontBacket, backBacket]);
     }
 
     constructor(props) {
@@ -70,6 +86,8 @@ export default class Game extends Component {
         };
     }
 
+    clickTime = 0
+
     componentWillMount() {
         this._panResponder = PanResponder.create({
             onStartShouldSetPanResponder: (evt, gestureState) => true,
@@ -77,38 +95,72 @@ export default class Game extends Component {
             onMoveShouldSetPanResponder: (evt, gestureState) => true,
             onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
             onPanResponderGrant: (evt, gestureState) => {
-                this.setState({
-                    gravity: 0,
-                });
-
-                Matter.Body.setAngularVelocity(this.body.body, 0);
-                Matter.Body.setVelocity(this.body.body, {x: 0, y: 0});
-
-                this.startPosition = {
-                    x: this.body.body.position.x,
-                    y: this.body.body.position.y,
-                }
+                // this.setState({
+                //     gravity: 0,
+                // });
+                //
+                Matter.Body.setAngularVelocity(this.body.body, -0.1);
+                Matter.Body.setVelocity(this.body.body, {x: 3, y: -20});
+                //
+                // this.startPosition = {
+                //     x: this.body.body.position.x,
+                //     y: this.body.body.position.y,
+                // }
             },
-            onPanResponderMove: (evt, gestureState) => {
-                Matter.Body.setPosition(this.body.body, {
-                    x: this.startPosition.x + gestureState.dx,
-                    y: this.startPosition.y + gestureState.dy,
-                });
-            },
+            onPanResponderMove: (evt, gestureState) => true,
             onPanResponderRelease: (evt, gestureState) => {
-                this.setState({
-                    gravity: 1,
-                });
-
-                Matter.Body.applyForce(this.body.body, {
-                    x: this.body.body.position.x,
-                    y: this.body.body.position.y,
-                }, {
-                    x: gestureState.vx,
-                    y: gestureState.vy,
-                });
+                // this.setState({
+                //     gravity: 1,
+                // });
+                //
+                // Matter.Body.applyForce(this.body.body, {
+                //     x: this.body.body.position.x,
+                //     y: this.body.body.position.y,
+                // }, {
+                //     x: gestureState.vx,
+                //     y: gestureState.vy,
+                // });
             },
         });
+
+        // this._panResponder = PanResponder.create({
+        //     onStartShouldSetPanResponder: (evt, gestureState) => true,
+        //     onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+        //     onMoveShouldSetPanResponder: (evt, gestureState) => true,
+        //     onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+        //     onPanResponderGrant: (evt, gestureState) => {
+        //         this.setState({
+        //             gravity: 0,
+        //         });
+        //
+        //         Matter.Body.setAngularVelocity(this.body.body, 0);
+        //         Matter.Body.setVelocity(this.body.body, {x: 0, y: 0});
+        //
+        //         this.startPosition = {
+        //             x: this.body.body.position.x,
+        //             y: this.body.body.position.y,
+        //         }
+        //     },
+        //     onPanResponderMove: (evt, gestureState) => {
+        //         Matter.Body.setPosition(this.body.body, {
+        //             x: this.startPosition.x + gestureState.dx,
+        //             y: this.startPosition.y + gestureState.dy,
+        //         });
+        //     },
+        //     onPanResponderRelease: (evt, gestureState) => {
+        //         this.setState({
+        //             gravity: 1,
+        //         });
+        //
+        //         Matter.Body.applyForce(this.body.body, {
+        //             x: this.body.body.position.x,
+        //             y: this.body.body.position.y,
+        //         }, {
+        //             x: gestureState.vx,
+        //             y: gestureState.vy,
+        //         });
+        //     },
+        // });
     }
 
     getBallStyles() {
@@ -140,23 +192,39 @@ export default class Game extends Component {
                     >
                         <Body
                             shape="circle"
-                            args={[dimensions.width/2-75/2, dimensions.height/2 - 75/2, 75]}//[初始x坐标，初始y坐标，半径] 因为是圆，所以是3个参数
+                            args={[dimensions.width/2-75/2, dimensions.height/2 - 75/2, 75/2]}//[初始x坐标，初始y坐标，半径] 因为是圆，所以是3个参数
                             density={0.003}
                             friction={1}
                             frictionStatic={0}
                             restitution={0.5}
                             ref={(b) => { this.body = b; }}
                         >
+                            <View
+                                style={this.getBallStyles()} {...this._panResponder.panHandlers}
+                            >
+                                <Image
+                                    source={require('./assets/basketball.png')}
+                                    height={75}
+                                    width = {75}
+                                />
+                            </View>
+                        </Body>
                         <View
-                            style={this.getBallStyles()} {...this._panResponder.panHandlers}
+                            style={{
+                                height:160,
+                                width:160,
+                                position:'absolute',
+                                right:0,
+                                top:200
+                            }}
                         >
                             <Image
-                                source={require('./assets/basketball.png')}
-                                height={75}
-                                width = {75}
+                                source={require('./assets/backet_100.png')}
+                                height={160}
+                                width = {160}
                             />
                         </View>
-                        </Body>
+
                     </World>
                 </Stage>
             </Loop>
